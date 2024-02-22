2dbab2f1d568f7fcfbd1c8efab67951dabe4b0bb
Teklu Mezgebo <mezge002@umn.edu>
2024-02-21 22:06:06 -0600

    reducercomponet created

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,29 @@
 // add code snippets from README
+let state 
+
+function reducer(state = {count: 0}, action) {
+    switch(action.type) {
+        case 'counter/increment':
+            return {count: state.count + 1}
+        default:
+            return state
+    }
+}
+
+function render() {
+    container = document.getElementById('container')
+    container.textContent = state.count
+}
+
+function dispatch(action) {
+    state = reducer(state, action)
+    render()
+}
+
+const button = document.getElementById('button')
+
+button.addEventListener(() => {
+    dispatch({type: 'counter/increment'})
+})
+
+dispatch({type: '@@INT'})
